3f50f2f15f2ff23edf77691c222be6f34adb928c
jackmford <fordjac@bvu.edu>
2020-04-21 12:13:58 -0500

    changes

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-#import firebase_admin
-from flask import Flask
-#from firebase_admin import credentials, db
-app = Flask(__name__)
+import os
+from flask import abort, Flask, json, redirect,\
+    render_template, request, Response, url_for, session, jsonify
+import firebase_admin
+from firebase_admin import credentials, db
 
-@app.route('/')
+
+app = Flask(__name__)
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+	'databaseURL':'https://capstone-test-curwsy.firebaseio.com/'
+})
+root=db.reference()
+
+def send_db(identifier, token):
+		#print(root.get()['jackf'])
+		print(root.child(identifier).set({'token' : token}))
+
+@app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    #cred = credentials.Certificate("serviceAccountKey.json")
-    #firebase_admin.initialize_app(cred, {
-        #'databaseURL' : 'https://capstone-test-curwsy.firebaseio.com/'
-    #})
-    #root = db.reference()
-    #print(root.child('users').get()['jack'])
-    print('testing')
-    return 'hello'
+  	print('testing')
+  	return render_template('index.html')
+
+@app.route('/db', methods=['POST', 'GET'])
+def db():
+		identifier = request.form.get('id')
+		token = request.form.get('token')
+		print(request.form.get('id'))
+		print(request.form.get('token'))
+		send_db(identifier, token)
+		return 'Thanks for signing up!'
